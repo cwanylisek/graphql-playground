998b5f61dee4c4d11f668716bd11764b03e17468
cwanylisek <rewiks@gmail.com>
2020-04-10 16:44:57 +0200

    set schema

--- server/app.js
+++ server/app.js
@@ -1,3 +1,12 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for reqiest on port 4000');
+});
