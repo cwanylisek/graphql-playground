897397c5d4234fa0fe307546ecb13bc7bc091d31
cwanylisek <rewiks@gmail.com>
2020-04-11 22:04:28 +0200

    make rootQuery, loadash

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+//dummy data 
+
+const books = [
+    { name: 'Inny swiat', genre: 'History', id: 1 },
+    { name: 'Sowieci', genre: 'History', id: 2 },
+    { name: 'Brave New World', genre: 'Fantasy', id: 3 },
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -10,3 +19,21 @@ const BookType = new GraphQLObjectType({
         genre: { type: GraphQLString }
     })
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                //code to get data from db/other source
+                return _.find(books, { id: args.id });
+            }
+        }
+    }
+});
+
+module.export = new GraphQLSchema({
+    query: RootQuery
+})
