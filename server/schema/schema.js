f0e9bd76ac31d710600b88079e9da2a7d8f59a15
cwanylisek <rewiks@gmail.com>
2020-04-13 23:24:47 +0200

    fix, authorsType && relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,23 +5,45 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLSchema,
-    GraphQLID
+    GraphQLID,
+    GraphQLInt
 } = graphql;
 
 //dummy data (db)
 
-var books = [
-    { name: 'Inny swiat', genre: 'History', id: 1 },
-    { name: 'Inny swiat', genre: 'History', id: 2 },
-    { name: 'Test', genre: 'Fantasy', id: 3 }
-]
+const books = [
+    { name: 'Inny swiat', genre: 'History', id: 1, authorId: 1 },
+    { name: 'Sowieci', genre: 'History', id: 2, authorId: 2 },
+    { name: 'Nowy wspanialy swiat', genre: 'Fantasy', id: 3, authorId: 3 }
+];
+
+const authors = [
+    { name: 'Gustaw Herling-Grudzinski', age: 81, id: 1 },
+    { name: 'Piotr Zychowicz', age: 40, id: 2 },
+    { name: 'Aldous Huxley', age: 69, id: 3 }
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                return authors.find(author => author.id == parent.authorId)
+            }
+        }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
     })
 });
 
@@ -33,7 +55,14 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 //code to get data from db
-                _.find(books, { id: args.id });
+                return books.find(book => book.id == args.id);
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return authors.find(author => author.id == args.id);
             }
         }
     }
