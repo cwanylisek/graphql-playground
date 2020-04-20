0ee0ca393c794933f112776e553862873f7b5a2c
cwanylisek <rewiks@gmail.com>
2020-04-20 22:52:50 +0200

    mongoose mutations

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// mongoose database connect
+mongoose.connect(
+    "mongodb+srv://cwanylisek:########@clustergraphql-scurx.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch(err => console.log("Error:", err.message));
 
 app.use('/graphql', graphqlHTTP({
     schema,
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 const {
     GraphQLObjectType,
@@ -35,7 +37,7 @@ const BookType = new GraphQLObjectType({
         author: {
             type: AuthorType,
             resolve(parent, args) {
-                return authors.find(author => author.id == parent.authorId)
+                //return authors.find(author => author.id == parent.authorId)
             }
         }
     })
@@ -50,7 +52,7 @@ const AuthorType = new GraphQLObjectType({
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                return books.filter(book => book.authorId == parent.id)
+                //return books.filter(book => book.authorId == parent.id)
             }
         }
     })
@@ -64,31 +66,52 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 //code to get data from db
-                return books.find(book => book.id == args.id);
+                //return books.find(book => book.id == args.id);
             }
         },
         author: {
             type: AuthorType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return authors.find(author => author.id == args.id);
+                //return authors.find(author => author.id == args.id);
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                return books
+                //return books
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args) {
-                return authors
+                //return authors
             }
         }
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                return author.save() // mongoose save
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
